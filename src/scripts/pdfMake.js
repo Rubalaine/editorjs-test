5e59c60a3416b3de67be337d580a5c8abf3a749f
unknown <harsonrichard13@gmail.com>
2020-09-27 10:38:13 +0100

    COMMENTS ADDED and some refactor on pdfMake.js

--- src/scripts/pdfMake.js
+++ src/scripts/pdfMake.js
@@ -1,17 +1,18 @@
+//SOME DEPENDECIES
 const pdfMake = require("pdfmake/build/pdfmake.js");
 const pdfFonts = require("pdfmake/build/vfs_fonts.js");
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export const printPDF = (doc) => {
-  const def = { content: [{ text: [] }] };
+  //
+  const documentDefinition = { content: [{ text: [] }] };
 
-  console.log("CONT", def.content[0]);
   doc.forEach((e) => {
-    console.log(e);
-    def.content[0].text.push(e);
+    //this block pushes those objects bolded and nonBolded that cames from index.js to the content array
+    //this is just pdfMake issues ... to see more feel free to go to their playground and seek for more than one piece
+    documentDefinition.content[0].text.push(e);
   });
 
-  console.log("FINAALLY", def.content[0].text);
-
-  pdfMake.createPdf(def).open();
+  //create a pdf and open it in a new browser tabe...with the open method
+  pdfMake.createPdf(documentDefinition).open();
 };
